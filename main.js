4a10537559204e973b3c08d27bd0596a0a698871
Wannes <wanneske_v@hotmail.com>
2019-01-19 17:24:29 +0100

    Added text

--- main.js
+++ main.js
@@ -11,6 +11,71 @@ function init(){
   camera.position.z = 5;
   camera.lookAt(new THREE.Vector3(0,0,0));
 
+  var loader = new THREE.FontLoader();
+  loader.load("font.json", function(font){
+    var font;
+    var color = 0xffdf00;
+    var material = new THREE.MeshBasicMaterial({
+      color: color,
+      side: THREE.DoubleSide
+    })
+    var message1 = "Make";
+    var message2 = "it";
+    var message3 = "rain";
+    var shapes = font.generateShapes(font, 100);
+    var geometry1 = new THREE.TextGeometry(message1.toUpperCase(), {
+      font: font,
+      size: 4,
+      height: 0
+    });
+    var geometry2 = new THREE.TextGeometry(message2.toUpperCase(), {
+      font: font,
+      size: 4,
+      height: 0
+    });
+    var geometry3 = new THREE.TextGeometry(message3.toUpperCase(), {
+      font: font,
+      size: 4,
+      height: 0
+    });
+
+    geometry1.computeBoundingBox();
+    geometry2.computeBoundingBox();
+    geometry3.computeBoundingBox();
+    geometry1.translate(0, 0, 0);
+    geometry2.translate(0, 0, 0);
+    geometry3.translate(0, 0, 0);
+
+    text1 = new THREE.Mesh(geometry1, material);
+    text2 = new THREE.Mesh(geometry2, material);
+    text3 = new THREE.Mesh(geometry3, material);
+
+    text1.position.x = -15;
+    text1.position.y = -5;
+    text1.position.z = -40;
+    text1.rotation.x = 0;
+    text1.rotation.y = .2;
+    text1.rotation.z = 0;
+
+    text2.position.x = -10;
+    text2.position.y = -10;
+    text2.position.z = -40;
+    text2.rotation.x = 0;
+    text2.rotation.y = .2;
+    text2.rotation.z = 0;
+
+    text3.position.x = -14;
+    text3.position.y = -15;
+    text3.position.z = -40;
+    text3.rotation.x = 0;
+    text3.rotation.y = .2;
+    text3.rotation.z = 0;
+
+    scene.add(text1);
+    scene.add(text2);
+    scene.add(text3);
+  });
+
   var particleGeo = new THREE.Geometry();
   var particleMat = new THREE.PointsMaterial({
     size: 5,
